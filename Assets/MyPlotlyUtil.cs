04a8d9238369931b5102504b938cafeb4cd09ad2
y_okada <yasokada@gmail.com>
2015-09-19 20:37:08 +0900

    refactor

--- Assets/MyPlotlyUtil.cs
+++ Assets/MyPlotlyUtil.cs
@@ -8,21 +8,21 @@ namespace NS_MyPlotlyUtil
 		public static string GetPostString(string username, string apikey) {
 			string res = 
 			"un=" + username + "&" +
-				"key=" + apikey + "&" +
-					"origin=plot&" +
-					"platform=lisp&" +
-					"args=[[0, 1, 2], [3, 4, 5], [1, 2, 3], [6, 6, 5]]&" +
-					"kwargs={\"filename\": \"plot from api\"," +
-					"\"fileopt\": \"overwrite\"," + 
-					"\"style\": {" + 
+			"key=" + apikey + "&" +
+			"origin=plot&" +
+			"platform=lisp&" +
+			"args=[[0, 1, 2], [3, 4, 5], [1, 2, 3], [6, 6, 5]]&" +
+			"kwargs={\"filename\": \"plot from api\"," +
+				"\"fileopt\": \"overwrite\"," + 
+				"\"style\": {" + 
 					"\"type\": \"bar\"" +
-					"}," + 
-					"\"traces\": [1]," + 
-					"\"layout\": {" +
+				"}," + 
+				"\"traces\": [1]," + 
+				"\"layout\": {" +
 					"\"title\": \"experimental data\"" +
-					"}," + 
-					"\"world_readable\": true" +
-					"}";
+				"}," + 
+				"\"world_readable\": true" +
+			"}";
 			return res;
 		}
 	}
