ff262ee69cab88a20138aed46d94ac808cbf3d69
y_okada <yasokada@gmail.com>
2015-09-19 20:35:25 +0900

    adding MyPlotlyUtil

---
+++ Assets/MyPlotlyUtil.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System.Net; // for Dns.XXX()
+
+namespace NS_MyPlotlyUtil
+{
+	public static class MyPlotlyUtil {
+		public static string GetPostString(string username, string apikey) {
+			string res = 
+			"un=" + username + "&" +
+				"key=" + apikey + "&" +
+					"origin=plot&" +
+					"platform=lisp&" +
+					"args=[[0, 1, 2], [3, 4, 5], [1, 2, 3], [6, 6, 5]]&" +
+					"kwargs={\"filename\": \"plot from api\"," +
+					"\"fileopt\": \"overwrite\"," + 
+					"\"style\": {" + 
+					"\"type\": \"bar\"" +
+					"}," + 
+					"\"traces\": [1]," + 
+					"\"layout\": {" +
+					"\"title\": \"experimental data\"" +
+					"}," + 
+					"\"world_readable\": true" +
+					"}";
+			return res;
+		}
+	}
+	
+}
---
+++ Assets/PostButtonScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // for Input Field
+
+using NS_MyPlotlyUtil; 
+
+public class PostButtonScript : MonoBehaviour {
+
+	public InputField IF_apikey;
+
+	public void PostButtonClick() {
+		StartCoroutine ("PostProcess");
+	}
+
+	IEnumerator PostProcess() {
+		string url = "https://plot.ly/clientresp";
+		string dataStr = MyPlotlyUtil.GetPostString ("7of9", IF_apikey.text);
+
+		byte [] data = System.Text.Encoding.UTF8.GetBytes (dataStr);
+		
+		WWW www = new WWW (url, data);
+		yield return www;
+		
+		int nop = 1; // for breakpoint
+	}
+
+}
